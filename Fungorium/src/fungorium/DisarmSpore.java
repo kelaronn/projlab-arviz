bf52dd170c4f78d5d5f00d1867ed15b5d3ecae85
Kelemen Áron Bence <k.aron.bence@gmail.com>
2025-03-20 12:22:35 +0100

    DisarmSpore added

--- Fungorium/src/fungorium/DisarmSpore.java
+++ Fungorium/src/fungorium/DisarmSpore.java
@@ -1,5 +1,13 @@
 package fungorium;
 
-public class DisarmSpore {
+public class DisarmSpore extends Spore {
 
+    public DisarmSpore(int nv, int ed, Fungus hf){
+        super(nv, ed, hf);
+    }
+
+    @Override public boolean giveEffect(Insect i){
+        i.setCutAbility(false);
+        return true;
+    }
 }
