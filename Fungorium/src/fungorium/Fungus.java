aee9cab7a782a3711654baeba76a9ef00960d0cd
MicsinaiAlex <micsinai.alex@gmail.com>
2025-04-18 22:04:25 +0200

    Modified Fungus and Hypha + add View and Controller interfaces.

--- Fungorium/src/fungorium/Fungus.java
+++ Fungorium/src/fungorium/Fungus.java
@@ -5,9 +5,10 @@ import java.util.List;
 
 /**
  * A Fungus osztály egy gombafajt reprezentál, amely gombatesteket (FungusBody) és gombafonalakat (Hypha)
- * csoportosít és tart számon. Felelőssége a gomba terjeszkedésének és túlélésének elősegítése.
+ * csoportosít és tart számon. Felelőssége a gomba terjeszkedésének és túlélésének elősegítése a tektonokon
+ * keresztül.
  */
-public class Fungus {
+public class Fungus implements IFungusView, IFungusController {
     /** A gombához tartozó gombatestek listája. */
     private List<FungusBody> bodies = new ArrayList<>();
 
@@ -15,53 +16,64 @@ public class Fungus {
     private List<Hypha> mycelium = new ArrayList<>();
 
     /**
-     * Hozzáad egy gombafonalat (Hypha) a mycelium listához.
+     * Hozzáad egy gombafonalat (Hypha) a micélium listához, ha az nem null, ehhez a gombához tartozik,
+     * és még nem szerepel a listában.
+     * 
      * @param h A hozzáadandó gombafonal.
+     * @return Igaz, ha a gombafonal hozzáadása sikeres, különben hamis.
      */
-    public void AddHypha(Hypha h) {
-        System.out.println(">[Hypha].GetHostFungus()");
+    public boolean AddHypha(Hypha h) {
         if(h != null && h.GetHostFungus().equals(this) && !mycelium.contains(h)) {
             mycelium.add(h);
-            System.out.println(">[Fungus].mycelium.add(h)");
+            return true;
         }
-
-
+        return false;
     }
 
     /**
-     * Eltávolít egy gombafonalat (Hypha) a mycelium listából.
+     * Eltávolít egy gombafonalat (Hypha) a micélium listából, ha az nem null, ehhez a gombához tartozik,
+     * és szerepel a listában.
+     * 
      * @param h Az eltávolítandó gombafonal.
+     * @return Igaz, ha a gombafonal eltávolítása sikeres, különben hamis.
      */
-    public void RemoveHypha(Hypha h) {
-        System.out.println(">[Hypha].GetHostFungus()");
+    public boolean RemoveHypha(Hypha h) {
         if(h != null && h.GetHostFungus().equals(this) && mycelium.contains(h)) {
             mycelium.remove(h);
-            System.out.println(">[Fungus].mycelium.remove(h)");
+            return true;
         }
+        return false;
     }
 
     /**
-     * Hozzáad egy gombatestet (FungusBody) a bodies listához.
+     * Hozzáad egy gombatestet (FungusBody) a gombatestek listájához, ha az nem null, ehhez a gombához
+     * tartozik, és még nem szerepel a listában.
+     * 
      * @param b A hozzáadandó gombatest.
+     * @return Igaz, ha a gombatest hozzáadása sikeres, különben hamis.
      */
-    public void AddBody(FungusBody b) {
-        System.out.println(">[FungusBody].GetHostFungus()");
+    public boolean AddBody(FungusBody b) {
         if(b != null && b.GetHostFungus().equals(this) && !bodies.contains(b)){
             bodies.add(b);
-            System.out.println(">[Fungus].bodies.add(b)");
+            return true;
         }
+        return false;
     }
 
     /**
-     * Eltávolít egy gombatestet (FungusBody) a bodies listából.
+     * Eltávolít egy gombatestet (FungusBody) a gombatestek listájából, ha az nem null, ehhez a gombához
+     * tartozik, és szerepel a listában. Az eltávolítás során a gombatestet a tektonról is törli.
+     * 
      * @param b Az eltávolítandó gombatest.
+     * @return Igaz, ha a gombatest eltávolítása sikeres, különben hamis.
      */
-    public void RemoveBody(FungusBody b) {
-        System.out.println(">[FungusBody].GetHostFungus()");
+    public boolean RemoveBody(FungusBody b) {
         if(b != null && b.GetHostFungus().equals(this) && bodies.contains(b)){
             bodies.remove(b);
-            System.out.println(">[Fungus].bodies.remove(b)");
+            b.GetTecton().SetFungusBody(null);
+            return true;
         }
+        return false;
     }
 
     /**
